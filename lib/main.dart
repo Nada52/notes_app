2553d02f5bb622468f4b09837ae97ea49357d6fb
nada elborhamy <73416301+Nada52@users.noreply.github.com>
2023-12-26 03:06:25 +0200

    register adapter

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:hive_flutter/hive_flutter.dart';
+import 'package:notes_app/models/note_model.dart';
 import 'package:notes_app/views/notes_view.dart';
 
 void main() async {
   await Hive.initFlutter();
   await Hive.openBox('notes_box');
+  Hive.registerAdapter(NoteModelAdapter());
   runApp(const NotesApp());
 }
-
 
 class NotesApp extends StatelessWidget {
   const NotesApp({super.key});
@@ -15,13 +16,9 @@ class NotesApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      theme: ThemeData(
-        brightness: Brightness.dark,
-        fontFamily: 'Poppins'
-      ),
+      theme: ThemeData(brightness: Brightness.dark, fontFamily: 'Poppins'),
       home: const NotesView(),
       debugShowCheckedModeBanner: false,
     );
   }
 }
-
