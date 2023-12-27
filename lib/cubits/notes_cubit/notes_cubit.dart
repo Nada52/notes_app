8c73927764b068486bc23e55350d6e72bdb5689e
nada elborhamy <73416301+Nada52@users.noreply.github.com>
2023-12-27 02:32:58 +0200

    create fetch notes method

--- lib/cubits/notes_cubit/notes_cubit.dart
+++ lib/cubits/notes_cubit/notes_cubit.dart
@@ -1,4 +1,5 @@
 import 'package:bloc/bloc.dart';
+import 'package:hive/hive.dart';
 import 'package:meta/meta.dart';
 import 'package:notes_app/models/note_model.dart';
 
@@ -6,4 +7,15 @@ part 'notes_state.dart';
 
 class NotesCubit extends Cubit<NotesState> {
   NotesCubit() : super(NotesInitial());
+
+  fetchAllNotes()async{
+  
+    try {
+      var notesBox = Hive.box<NoteModel>('notes_box');
+      List<NoteModel> notes =  notesBox.values.toList();
+      emit(NotesSuccess(notes:  notes));
+    } catch (e) {
+      emit(NotesFailure(errMessage: e.toString()));
+    }
+  }
 }
--- lib/views/notes_view.dart
+++ lib/views/notes_view.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:notes_app/cubits/notes_cubit/notes_cubit.dart';
 import 'package:notes_app/views/widgets/custom_add_note_bottom_sheet.dart';
 import 'package:notes_app/views/widgets/notes_view_body.dart';
 
@@ -7,23 +9,26 @@ class NotesView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      // يعني متغيرش من حجم الشاشة علشان تناسب الكيبورد
-      // resizeToAvoidBottomInset: false,
-      body: const NotesViewBody(),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {
-          showModalBottomSheet(
-            isScrollControlled: true,
-            context: context,
-            builder: (context) => const CustomAddNoteBottomSheet(),
-          );
-        },
-        backgroundColor: Colors.tealAccent,
-        shape: const CircleBorder(),
-        child: const Icon(
-          Icons.add,
-          color: Colors.black,
+    return BlocProvider(
+      create: (context) => NotesCubit(),
+      child: Scaffold(
+        // يعني متغيرش من حجم الشاشة علشان تناسب الكيبورد
+        // resizeToAvoidBottomInset: false,
+        body: const NotesViewBody(),
+        floatingActionButton: FloatingActionButton(
+          onPressed: () {
+            showModalBottomSheet(
+              isScrollControlled: true,
+              context: context,
+              builder: (context) => const CustomAddNoteBottomSheet(),
+            );
+          },
+          backgroundColor: Colors.tealAccent,
+          shape: const CircleBorder(),
+          child: const Icon(
+            Icons.add,
+            color: Colors.black,
+          ),
         ),
       ),
     );
